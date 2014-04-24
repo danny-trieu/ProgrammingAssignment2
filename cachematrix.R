22c03919398e92afd82772bf8bbae2e96c776f6d
Danny Trieu <trieu.danny@gmail.com>
2014-04-24 10:19:25 -0700

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Using R's scoping rule, this function cached the inverse matrix 
+## inside R's object up one its initial request.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function return a wrapper around a given matrix(m) with accessor
+## methods to its attributes, matrix and its inverse.
+## =====================================================================
+makeCacheMatrix <- function(m = matrix()) {
+    # cached inverse of x
+    cachedInverse <- NULL
+    
+    # Set the matrix attribute
+    set <- function(newM) {
+        m <<- newM
+        cachedInverse <<- NULL
+    }
+    
+    # Get the matrix attribute
+    get <- function() {
+        m
+    }
+    
+    # Set the cached inverse of the matrix
+    setInverse <- function(newInverse) {
+        cachedInverse <<- newInverse
+    }
+    
+    # Get the cached inverse of the matrix attribute
+    getInverse <- function() {
+        cachedInverse
+    }
+    
+    # Manifest all accessor methods
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache.
+## ===========================================================================
+cacheSolve <- function(m, ...) {
+    cachedInverse <- m$getInverse()
+    
+    toReturn <- if (!is.null(cachedInverse)) {
+        message("getting cached inverse matrix")
+        cachedInverse 
+    }
+    else {
+        inversed <- solve(m$get())
+        m$setInverse(inversed)
+        inversed
+    }
 }
